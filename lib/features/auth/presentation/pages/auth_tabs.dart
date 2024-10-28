71f5646d59eb959da3c8b5ba0e885c7ef9abb648
Heurtault Aubin <aub.heurtault@gmail.com>
2024-10-28 13:39:45 +0100

    tabs auth

--- lib/features/auth/presentation/pages/auth_tabs.dart
+++ lib/features/auth/presentation/pages/auth_tabs.dart
@@ -1,38 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 import 'package:modular_ui/modular_ui.dart';
 
 class AuthTabs extends StatelessWidget {
-  final int initialIndex;
+  final ValueNotifier<int> selectedIndex;
+  final ValueChanged<int> onTabChanged;
 
-  const AuthTabs({Key? key, required this.initialIndex}) : super(key: key);
+  const AuthTabs({
+    super.key,
+    required this.selectedIndex,
+    required this.onTabChanged,
+  });
 
   @override
   Widget build(BuildContext context) {
-    ValueNotifier<int> tabIndexNotifier = ValueNotifier(initialIndex);
-
-    return ValueListenableBuilder<int>(
-      valueListenable: tabIndexNotifier,
-      builder: (context, tabIndex, child) {
-        return Container(
-          width: double.infinity, // Ensure full width
-          child: MUITabs.underlined(
-            tabIndex: tabIndex,
-            onChanged: (index) {
-              tabIndexNotifier.value = index;
-              if (index == 0) {
-                context.go('/login'); // Navigate to Login page
-              } else {
-                context.go('/register'); // Navigate to Register page
-              }
-            },
-            tabs: [
-              MUITab(label: "Login"),
-              MUITab(label: "Register"),
-            ],
-          ),
-        );
-      },
+    return MUITabs.underlined(
+      tabIndex: selectedIndex.value,
+      onChanged: onTabChanged,
+      tabs: [
+        MUITab(label: "Login"),
+        MUITab(label: "Register"),
+      ],
     );
   }
 }
