f75010dee087a22c7838e625b53f77193d5dee2d
Heurtault Aubin <aub.heurtault@gmail.com>
2024-10-28 13:41:21 +0100

    update router

--- lib/core/app/router.dart
+++ lib/core/app/router.dart
@@ -3,8 +3,7 @@ import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:wolkup_app/features/auth/auth.dart';
-import 'package:wolkup_app/features/auth/presentation/pages/login_page.dart';
-import 'package:wolkup_app/features/auth/presentation/pages/register_page.dart';
+import 'package:wolkup_app/features/auth/presentation/pages/auth_page.dart'; // Nouvelle page unique pour Auth
 import 'package:wolkup_app/features/auth/presentation/states/auth.dart';
 import 'package:wolkup_app/core/ui/ui.dart';
 import 'router_notifier.dart';
@@ -16,7 +15,7 @@ GoRouter router(RouterRef ref) {
   final auth = ref.watch(authProvider);
 
   return GoRouter(
-    initialLocation: '/login',
+    initialLocation: '/auth',
     refreshListenable: RouterNotifier(ref),
     routes: [
       GoRoute(
@@ -24,32 +23,24 @@ GoRouter router(RouterRef ref) {
         builder: (context, state) => const AppLayout(),
       ),
       GoRoute(
-        path: '/login',
-        builder: (context, state) => const LoginPage(),
-      ),
-      GoRoute(
-        path: '/register',
-        builder: (context, state) => const RegisterPage(),
+        path: '/auth',
+        builder: (context, state) => const AuthPage(), // Affiche la page unique AuthPage
       ),
     ],
     redirect: (context, state) {
       final isAuthenticated = !auth.isEmpty;
       debugPrint('isAuth: $isAuthenticated, path: ${state.fullPath}');
 
-      if (!isAuthenticated &&
-          (state.fullPath == '/login' || state.fullPath == '/register')) {
+      if (!isAuthenticated && state.fullPath == '/auth') {
         return null;
       }
 
-      if (isAuthenticated &&
-          (state.fullPath == '/login' || state.fullPath == '/register')) {
+      if (isAuthenticated && state.fullPath == '/auth') {
         return '/';
       }
 
-      if (!isAuthenticated &&
-          state.fullPath != '/login' &&
-          state.fullPath != '/register') {
-        return '/login';
+      if (!isAuthenticated && state.fullPath != '/auth') {
+        return '/auth';
       }
 
       return null;
