1241ef641ec65e19ab122209d1e1fd665fa0765e
Heurtault Aubin <aub.heurtault@gmail.com>
2025-01-07 09:19:17 +0100

    update

--- lib/core/app/app.dart
+++ lib/core/app/app.dart
@@ -1,19 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:wolkup_app/core/app/router.dart';
-import '../../themes/material_theme.dart';
-import '../../themes/theme_provider.dart';
+import 'package:wolkup_app/themes/theme_provider.dart';
 
 class App extends ConsumerWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    // Récupération du thème via Riverpod
     final theme = ref.watch(themeProvider);
+    // Récupération du router via Riverpod
     final router = ref.watch(routerProvider);
 
     return MaterialApp.router(
-      theme: theme,
+      debugShowCheckedModeBanner: false,
+      title: 'Wolkup App',
+      theme: theme, // Thème dynamique
       routeInformationParser: router.routeInformationParser,
       routerDelegate: router.routerDelegate,
       routeInformationProvider: router.routeInformationProvider,
